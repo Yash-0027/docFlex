07a914963a9cd0b8a8e9c2f164051bfc975f2ff9
Yash-0027 <yashj7438@gmail.com>
2024-10-23 17:36:43 +0530

    file was added

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,47 @@
+// import React from "react";
+// import Navbar from "../components/Navbar";
+// import img from "../Images/img1.jpg";
+// import { Link } from "react-router-dom";
+
+// const MainPage = () => {
+//   return (
+//     <div className="">
+//       <Navbar />
+//       <div className="  flex justify-center flex-row gap-5 ">
+//         <div  className="lfet w-[50vw] ">
+//           <h2 className="text-[45px] text-balance  flex justify-center items-center p-5 text-center">
+//             "Save, Organize, and Access Your Documents Anytime"
+//           </h2>
+//          <Link to="/login">
+//          <button>
+//             Login
+//           </button>
+//          </Link>
+          
+          
+          
+          
+//          <Link to="/signup">
+//          <button>
+//             Login
+//           </button>
+//          </Link>
+
+//           <p className="flex justify-center items-center flex-row mt-10 p-5 text-[20px] mb-0 text-center text-balance ">
+//             Doc Saver allows you to safely store, manage, and access your
+//             important documents online. With cloud storage and an
+//             easy-to-navigate interface, Doc Saver ensures your files are backed
+//             up securely and are available whenever you need them...
+            
+           
+//           </p>
+//           <p className="flex justify-center items-center flex-row  p-5 text-[20px] text-center"> Doc Saver makes document storage and retrieval simple and secure. Keep all your essential files in one place, protected by advanced encryption. Whether for personal or professional use, Doc Saver streamlines document management so you can stay organized and worry-free.</p>
+//         </div>
+//         <div className="right mt-10  ">
+//           <img className="w-[700px] h-[550px] mx-auto"src={img} alt="img "/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default MainPage;
